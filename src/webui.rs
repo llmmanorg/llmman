1329d04c6244e7c31d361d5fff1ef4124c0cb7d1
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2026-08-27 19:07:21 +0300

    fix(webui): remove blank line between doc comment and item
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- src/webui.rs
+++ src/webui.rs
@@ -3,7 +3,6 @@
 /// Each constant holds the raw gzip-compressed bytes for the corresponding
 /// file from the `webui/` directory.  Serve them with the headers
 /// `Content-Encoding: gzip` and the appropriate `Content-Type`.
-
 pub static INDEX_HTML: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/webui_gz/index.html.gz"));
 
 pub static BUNDLE_JS: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/webui_gz/bundle.js.gz"));
